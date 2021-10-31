ce95f1be9b5d77f97947e590100d8941d8f7176a
reo-androider <reo.runningg@gmail.com>
2021-10-31 21:15:42 +0900

    create entity

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,7 +5,7 @@ fun main() {}
 object Users : IntIdTable("user")
 
 object Questions : IntIdTable() {
-    val ward = varchar("word", 4)
+    val word = varchar("word", 4)
     val answer = varchar("answer", 4)
     val description = varchar("description", 20)
     val chapterId = reference("chapterId", Chapters)
--- src/main/kotlin/entity/MyEntity.kt
+++ src/main/kotlin/entity/MyEntity.kt
@@ -5,3 +5,25 @@ import org.jetbrains.exposed.dao.id.EntityID
 class User(id: EntityID<Int>) : IntEntity(id) {
     companion object : IntEntityClass<User>(Users)
 }
+
+class Question(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<Question>(Questions)
+
+    var word by Questions.word
+    var answer by Questions.answer
+    var description by Questions.description
+    var chapterId by Chapter referencedOn Questions.chapterId
+}
+
+class Mistake(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<Mistake>(Mistakes)
+
+    var userId by User referencedOn Mistakes.userId
+}
+
+class Chapter(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<Chapter>(Chapters)
+
+    var title by Chapters.title
+    var count by Chapters.count
+}
