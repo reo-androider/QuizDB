2a0137abedf7e8b186c4b014947c60c816591762
reo-androider <reo.runningg@gmail.com>
2021-10-31 09:45:20 +0900

    create table

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,39 +1,23 @@
-import org.jetbrains.exposed.dao.IntEntity
-import org.jetbrains.exposed.dao.IntEntityClass
-import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IntIdTable
-import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.SchemaUtils
-import org.jetbrains.exposed.sql.StdOutSqlLogger
-import org.jetbrains.exposed.sql.addLogger
-import org.jetbrains.exposed.sql.transactions.transaction
 
 fun main() {
-    //an example connection to H2 DB
-    Database.connect("jdbc:h2:mem:test", driver = "org.h2.Driver")
 
-    transaction {
-        // print sql to std-out
-        addLogger(StdOutSqlLogger)
-
-        SchemaUtils.create(Cities)
-
-        // insert new city. SQL: INSERT INTO Cities (name) VALUES ('St. Petersburg')
-        City.new {
-            name = "St. Petersburg"
-        }
-
-        // 'select *' SQL: SELECT Cities.id, Cities.name FROM Cities
-        println("Cities: ${City.all()}")
-    }
 }
 
-object Cities : IntIdTable() {
-    val name = varchar("name", 50)
+object User : IntIdTable("user")
+
+object Question : IntIdTable() {
+    val ward = varchar("word", 4)
+    val answer = varchar("answer", 4)
+    val description = varchar("description", 20)
+    val chapterId = reference("chapterId", Chapter)
 }
 
-class City(id: EntityID<Int>) : IntEntity(id) {
-    companion object : IntEntityClass<City>(Cities)
+object Mistake : IntIdTable() {
+    val userId = reference("userId", User)
+}
 
-    var name by Cities.name
+object Chapter : IntIdTable() {
+    val title = varchar("title", 10)
+    val count = integer("count")
 }
