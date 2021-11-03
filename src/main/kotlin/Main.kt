cd15d854ed02cac849b7684b216c2e5d96a7ab32
reo-androider <reo.runningg@gmail.com>
2021-11-04 08:46:36 +0900

    create table

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,9 @@
+import model.table.Chapters
+import model.table.Mistakes
+import model.table.Questions
+import model.table.Users
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils.create
 import org.jetbrains.exposed.sql.StdOutSqlLogger
 import org.jetbrains.exposed.sql.addLogger
 import org.jetbrains.exposed.sql.transactions.transaction
@@ -6,6 +11,7 @@ import org.jetbrains.exposed.sql.transactions.transaction
 fun main() {
     Database.connect("jdbc:postgresql://localhost:12346/test", "org.postgresql.Driver", "root", "your_pwd")
     transaction {
+        create(Users, Questions, Mistakes, Chapters)
         addLogger(StdOutSqlLogger)
     }
 }
