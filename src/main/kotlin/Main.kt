928a980e9ee02284c9b71ee229458fbeae546152
reo-androider <reo.runningg@gmail.com>
2021-11-03 21:56:41 +0900

    add logger

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,11 @@
-import model.table.Chapters
-import model.table.Mistakes
-import model.table.Questions
-import model.table.Users
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.StdOutSqlLogger
+import org.jetbrains.exposed.sql.addLogger
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun main() {
     Database.connect("jdbc:postgresql://localhost:12346/test", "org.postgresql.Driver", "root", "your_pwd")
     transaction {
-        create(Users, Questions, Mistakes, Chapters)
+        addLogger(StdOutSqlLogger)
     }
 }
