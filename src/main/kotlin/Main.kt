9fbff98ffb24ebd1b52a041b844c8dcbb51909ac
reo-androider <reo.runningg@gmail.com>
2021-11-03 21:52:30 +0900

    add database connect

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,10 +2,11 @@ import model.table.Chapters
 import model.table.Mistakes
 import model.table.Questions
 import model.table.Users
+import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.transactions.transaction
-import java.beans.EventHandler.create
 
 fun main() {
+    Database.connect("jdbc:postgresql://localhost:12346/test", "org.postgresql.Driver", "root", "your_pwd")
     transaction {
         create(Users, Questions, Mistakes, Chapters)
     }
