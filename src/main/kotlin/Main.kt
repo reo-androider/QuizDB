a9c5cf4c523a99f6efae8e2bbaa8e6187e62851b
reo-androider <reo.runningg@gmail.com>
2021-11-03 21:00:48 +0900

    divide table

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,7 @@
-import org.jetbrains.exposed.dao.id.IntIdTable
+import model.table.Chapters
+import model.table.Mistakes
+import model.table.Questions
+import model.table.Users
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.beans.EventHandler.create
 
@@ -6,22 +9,4 @@ fun main() {
     transaction {
         create(Users, Questions, Mistakes, Chapters)
     }
-}
-
-object Users : IntIdTable("user")
-
-object Questions : IntIdTable() {
-    val word = varchar("word", 4)
-    val answer = varchar("answer", 4)
-    val description = varchar("description", 20)
-    val chapterId = reference("chapterId", Chapters)
-}
-
-object Mistakes : IntIdTable() {
-    val userId = reference("userId", Users)
-}
-
-object Chapters : IntIdTable() {
-    val title = varchar("title", 10)
-    val count = integer("count")
 }
