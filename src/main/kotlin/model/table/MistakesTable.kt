3e5387c470a7fe9f28fb17c8700f4bc0d16c9e88
reo-androider <reo.runningg@gmail.com>
2021-11-04 19:10:05 +0900

    add question id column

--- src/main/kotlin/model/table/MistakesTable.kt
+++ src/main/kotlin/model/table/MistakesTable.kt
@@ -4,4 +4,5 @@ import org.jetbrains.exposed.dao.id.IntIdTable
 
 object Mistakes : IntIdTable() {
     val userId = reference("userId", Users)
+    val questionId = reference("questionId", Questions)
 }
