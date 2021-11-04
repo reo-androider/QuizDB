4acb911e203f9fed0ca59c3b7d41ee0826a120c3
reo-androider <reo.runningg@gmail.com>
2021-11-04 18:46:38 +0900

    isolate chapters table

---
+++ src/main/kotlin/model/table/ChaptersTable.kt
@@ -0,0 +1,8 @@
+package model.table
+
+import org.jetbrains.exposed.dao.id.IntIdTable
+
+object Chapters : IntIdTable() {
+    val title = varchar("title", 10)
+    val count = integer("count")
+}
--- src/main/kotlin/model/table/QuizTable.kt
+++ src/main/kotlin/model/table/QuizTable.kt
@@ -1,9 +1,3 @@
 package model.table
 
-import org.jetbrains.exposed.dao.id.IntIdTable
 
-
-object Chapters : IntIdTable() {
-    val title = varchar("title", 10)
-    val count = integer("count")
-}
