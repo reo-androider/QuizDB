502c3a18f3fe8645363353f0603a89b13002337c
reo-androider <reo.runningg@gmail.com>
2021-11-04 19:01:25 +0900

    add user column

--- src/main/kotlin/model/table/UsersTable.kt
+++ src/main/kotlin/model/table/UsersTable.kt
@@ -2,4 +2,7 @@ package model.table
 
 import org.jetbrains.exposed.dao.id.IntIdTable
 
-object Users : IntIdTable("user")
+object Users : IntIdTable() {
+    val name = varchar("name", 20)
+    val mail = varchar("mail", 255)
+}
