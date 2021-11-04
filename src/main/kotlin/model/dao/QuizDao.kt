ca08b1b2138db16a626b6540b422540ef86765c7
reo-androider <reo.runningg@gmail.com>
2021-11-04 18:50:29 +0900

    isolate user dao

--- src/main/kotlin/model/dao/QuizDao.kt
+++ src/main/kotlin/model/dao/QuizDao.kt
@@ -1,14 +1,9 @@
 import model.table.Chapters
 import model.table.Mistakes
 import model.table.Questions
-import model.table.Users
 import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
-
-class User(id: EntityID<Int>) : IntEntity(id) {
-    companion object : IntEntityClass<User>(Users)
-}
 
 class Question(id: EntityID<Int>) : IntEntity(id) {
     companion object : IntEntityClass<Question>(Questions)
