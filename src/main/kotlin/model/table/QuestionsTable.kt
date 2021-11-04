21c5bd26a39ee10dd0f3a2d462a7db4291c1700e
reo-androider <reo.runningg@gmail.com>
2021-11-04 18:45:52 +0900

    isolate questions table

---
+++ src/main/kotlin/model/table/QuestionsTable.kt
@@ -0,0 +1,10 @@
+package model.table
+
+import org.jetbrains.exposed.dao.id.IntIdTable
+
+object Questions : IntIdTable() {
+    val word = varchar("word", 4)
+    val answer = varchar("answer", 4)
+    val description = varchar("description", 255)
+    val chapterId = reference("chapterId", Chapters)
+}
