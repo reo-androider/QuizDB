2d6e57c31841a3461b6b643f43e01083c55b1783
reo-androider <reo.runningg@gmail.com>
2021-11-04 18:52:26 +0900

    isolate chapter dao

---
+++ src/main/kotlin/model/dao/ChapterDao.kt
@@ -0,0 +1,13 @@
+package model.dao
+
+import model.table.Chapters
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
+
+class Chapter(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<Chapter>(Chapters)
+
+    var title by Chapters.title
+    var count by Chapters.count
+}
--- src/main/kotlin/model/dao/QuizDao.kt
+++ src/main/kotlin/model/dao/QuizDao.kt
@@ -1,12 +1,3 @@
-import model.table.Chapters
-import org.jetbrains.exposed.dao.IntEntity
-import org.jetbrains.exposed.dao.IntEntityClass
-import org.jetbrains.exposed.dao.id.EntityID
 
 
-class Chapter(id: EntityID<Int>) : IntEntity(id) {
-    companion object : IntEntityClass<Chapter>(Chapters)
 
-    var title by Chapters.title
-    var count by Chapters.count
-}
