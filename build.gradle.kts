2bc878bab018008fe62a908bb700e914ace1976f
reo-androider <reo.runningg@gmail.com>
2021-11-05 21:17:09 +0900

    plugin ktor

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ val exposedVersion: String by project
 val psql: String by project
 val h2: String by project
 val slf4f: String by project
+val kotlinVersion: String by project
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test:1.5.31")
     implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
@@ -24,6 +25,12 @@ dependencies {
     implementation("org.postgresql:postgresql:$psql")
     implementation("com.h2database:h2:$h2")
     implementation("org.slf4j:slf4j-simple:$slf4f")
+    implementation("io.ktor:ktor-server-netty:$kotlinVersion")
+    implementation("io.ktor:ktor-server-core:$kotlinVersion")
+    implementation("io.ktor:ktor-client-core:$kotlinVersion")
+    implementation("io.ktor:ktor-client-core-jvm:$kotlinVersion")
+    implementation("io.ktor:ktor-client-apache:$kotlinVersion")
+    testImplementation("io.ktor:ktor-server-tests:$kotlinVersion")
 }
 
 tasks.test {
