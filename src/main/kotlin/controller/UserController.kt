43d41500d807f6fcb5b701248549561838f18cae
reo-androider <reo.runningg@gmail.com>
2021-11-23 11:18:13 +0900

    add receive request

--- src/main/kotlin/controller/UserController.kt
+++ src/main/kotlin/controller/UserController.kt
@@ -1,14 +1,16 @@
 package controller
 
 import io.ktor.application.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import model.deserialize.CreateUserAccount
 
 fun Route.userController() {
     get("/user") {
         call.respondText { "user routing ok" }
     }
     post {
-
+        val request = call.receive<CreateUserAccount>()
     }
 }
