1a46818d3a2e6414b0d65c48ad20e086381eb92a
reoru <reo.runningg@gmail.com>
2022-02-22 16:05:31 +0900

    missed rm

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,32 @@
+import controller.userController
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import model.table.Chapters
+import model.table.Mistakes
+import model.table.Questions
+import model.table.Users
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils.create
+import org.jetbrains.exposed.sql.StdOutSqlLogger
+import org.jetbrains.exposed.sql.addLogger
+import org.jetbrains.exposed.sql.transactions.transaction
+
+fun main() {
+    Database.connect("jdbc:h2:mem:test", driver = "org.h2.Driver")
+    transaction {
+        addLogger(StdOutSqlLogger)
+        create(Users, Questions, Mistakes, Chapters)
+    }
+    val server = embeddedServer(Netty, port = 8080) {
+        routing {
+            userController()
+            get("/") {
+                call.respondText("success!")
+            }
+        }
+    }
+    server.start(wait = true)
+}
